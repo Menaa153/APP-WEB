a5ff1a84705e3ed3875a82a71435533421bde74f
Menaa153 <cimbacuan@unal.edu.co>
2024-08-23 01:49:11 -0500

    commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn confecciones.wsgi --log-file -
--- confecciones/settings.py
+++ confecciones/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,12 @@ SECRET_KEY = 'django-insecure-@bxff9!jv^@p*24#e*^9o6gs53b)-ixwc%!$@mc8d7^s(h(nv9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'confeccionesleidy-pk0zgj19e-cristians-projects-c48b555a.vercel.app',
+    'localhost',
+    '127.0.0.1',
+]
+
 
 
 # Application definition
@@ -75,8 +84,12 @@ WSGI_APPLICATION = 'confecciones.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres.qyzyelhmumvmgrehirch'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'leidyconf123.'),
+        'HOST': os.environ.get('DB_HOST', 'aws-0-us-west-1.pooler.supabase.com'),
+        'PORT': os.environ.get('DB_PORT', '6543'),
     }
 }
 
